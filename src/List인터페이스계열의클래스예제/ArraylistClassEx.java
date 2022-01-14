383c4ab3ae2ef2764606960b9378693cf98c5c4c
JongIn Yoon <bellpp93@gmail.com>
2022-01-14 17:23:27 +0900

    0114_Hex

--- src/List인터페이스계열의클래스예제/ArraylistClassEx.java
+++ src/List인터페이스계열의클래스예제/ArraylistClassEx.java
@@ -2,13 +2,37 @@ package List인터페이스계열의클래스예제;
 
 import java.util.ArrayList;
 import java.util.List;
+//import java.lang.Integer;  //java.lang 패키지는 import 하지 않아도 자동적으로 포함된다.
 
 public class ArraylistClassEx {
 
 	public static void main(String[] args) {
 		//가변배열 객체생성	<E>는 제네릭 기능 => Element(요소)
 		//list<객체타입> 참조변수 = new 생성자<객체타입>(매개변수1, 매개변수2);  => 가변배열 생성하는 공식
-		List<Integer> list = new ArrayList<Integer>();  //기본 10개의 공간을 확보함!!
+		List<Integer> list = new ArrayList<Integer>();	//기본 10개의 공간을 확보함!!
+		
+		//객체 추가
+//		list.add(new Integer(90));
+		list.add(90);	//JDK1.5 버전부터 가능 => 자동박싱(boxing) /* 정수를 객체화 시킨 것이기 때문에 정수가 아니다.
+		list.add(85);
+		list.add(100);
+		list.add(70);
+		list.add(95);
+		System.out.println(list);
+		//[90, 85, 100, 70, 95]
+		
+		list.add(80);
+		System.out.println(list);
+		//[90, 85, 100, 70, 95, 80]
+		
+		list.add(1, 60);
+		System.out.println(list);
+		//[90, 60, 85, 100, 70, 95, 80]
+		
+		list.set(3, 65);
+		System.out.println(list);
+		//[90, 60, 85, 65, 70, 95, 80]
+		
 	}
 
 }
---
+++ src/Set인터페이스계열의클래스예제/Member.java
@@ -0,0 +1,32 @@
+package Set인터페이스계열의클래스예제;
+
+//			자식클래스 extends 부모클래스
+public class Member extends Object{
+	//멤버변수(=인스턴스 변수)
+	public String name;		//이름
+	public int age;			//나이
+	public String juminNum;	//주민번호
+	
+	//생성자				매개변수(parameter) => 멤버변수보다 매개변수를 우선적으로 처리한다.
+	public Member(String name, int age, String juminNum) {
+		this.name = name;
+		this.age = age;
+		this.juminNum = juminNum;
+	}
+	//부모클래스의 메소드를 수동으로 오버라이드 시키는 방법
+	//	=> 먼저 e를 입력하고 Ctrl + Spacebar 동시에 누르면 메소드가 뜬다. 더블 클릭한다.
+	@Override  //어노테이션
+	public boolean equals(Object obj) {
+		if (obj instanceof Member) {
+			Member member = (Member) obj;
+			return member.name.equals(name) && (member.age == age)
+					&& member.juminNum.equals(juminNum);
+		}else {
+			return false;
+		}
+	}
+	@Override
+	public int hashCode() {
+		return name.hashCode() + age + juminNum.hashCode();
+	}
+}
